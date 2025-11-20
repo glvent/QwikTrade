4a58439f6283ebca79ffb5a2c4add2e23f29eb71
George Vent <george3rd43@gmail.com>
2025-11-19 19:48:36 -0600

    fix fake data logic

--- src/app.rs
+++ src/app.rs
@@ -14,13 +14,18 @@ pub struct App {
 impl Default for App {
     fn default() -> Self {
         // Placeholder data...
-        // Eventually, symbol and timeframe will be abstracted...
+        // Eventually, symbol and timeframe might be abstracted...
+        let mut market = MarketState::new(30_000.0);
+        
+        // Generate all bars immediately at startup
+        market.generate_fake_data(1_000);
+        
         App {
             symbol: "BTC-USDT".to_owned(),
             timeframe: "1m".to_owned(),
             status_message: "Disconnected".to_owned(),
             tick_counter: 0,
-            market: MarketState::new(30_000.0),
+            market,
         }
     }
 }
@@ -29,12 +34,11 @@ impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         self.tick_counter += 1;
 
-        self.market.generate_fake_data(1_000);
-
         layout::draw_top_panel(self, ctx);
         layout::draw_left_panel(self, ctx);
         layout::draw_central_panel(self, ctx);
-
+        //layout::draw_right_panel(self, ctx);
+        
         ctx.request_repaint();
     }
 }
--- src/ui/layout.rs
+++ src/ui/layout.rs
@@ -4,6 +4,7 @@ use crate::app::App;
 use crate::ui::components::chart::draw_chart;
 use crate::ui::components::top_toolbar::draw_top_toolbar;
 use crate::ui::components::left_toolbar::draw_left_toolbar;
+// use crate::ui::components::right_toolbar::draw_right_toolbar;
 
 pub fn draw_central_panel(app: &mut App, ctx: &egui::Context) {
     egui::CentralPanel::default().show(ctx, |ui| {
@@ -22,8 +23,17 @@ pub fn draw_top_panel(app: &mut App, ctx: &egui::Context) {
 pub fn draw_left_panel(app: &mut App, ctx: &egui::Context) {
     egui::SidePanel::left("left_panel")
         .resizable(true)
-        .default_width(220.0)
+        .default_width(225.0)
         .show(ctx, |ui| {
             draw_left_toolbar(ui);
+        });
+}
+
+pub fn _draw_right_panel(_app: &mut App, ctx: &egui::Context) {
+    egui::SidePanel::right("right_panel")
+        .resizable(true)
+        .default_width(225.0)
+        .show(ctx, |ui| {
+            // TODO: Implement right panel...
         });
 }
